e5b4399d2972e40da65616f17f3bd6f8e218ff1d
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-04-11 13:40:33 +0300

    added user model and added env to gitignore

--- app/models.py
+++ app/models.py
@@ -1,2 +1,13 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String, index=True, unique=True)
+    email = db.Column(db.String, index=True, unique=True)
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
