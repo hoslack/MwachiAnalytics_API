7bf9dd02d8ba7bfdf53f3ac7b4633613c0eccf6e
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-04-11 17:02:04 +0300

    Started created endpoint for login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import request, jsonify
+from flask import request, jsonify, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 from app.models import User
@@ -18,4 +18,30 @@ def register_user():
     user = User(public_id=str(uuid.uuid4()), username=data['username'], email=data['email'], password=hashed_password)
     user.save()
     return jsonify({'message': 'User created in the database'})
+
+
+@app.route('/user/<public_id>', methods=['GET'])
+def get_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+    output = []
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['username'] = user.username
+    user_data['email'] = user.email
+    output.append(user_data)
+    return jsonify({'data': output})
+
+
+@app.route('/auth/login', methods=['POST'])
+def login():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic-realm="Login Required!"'})
+    user = User.query.filter_by(username=auth.username).first()
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic-realm="Login Required!"'})
+    if check_password_hash(user.password,auth.password):
+
+
+
 
