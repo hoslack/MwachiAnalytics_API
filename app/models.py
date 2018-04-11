57e1cd278cc6349d24b2779ae575cc53ff5aa81b
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-04-11 15:03:46 +0300

    created first functional rest endpoint for registration

--- app/models.py
+++ app/models.py
@@ -7,7 +7,11 @@ class User(db.Model):
     username = db.Column(db.String, index=True, unique=True)
     email = db.Column(db.String, index=True, unique=True)
     password = db.Column(db.String(80))
-    admin = db.Column(db.Boolean)
+
+    """Method for saving user into the database user """
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
 from app import app
+from flask import request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    user = User(public_id=str(uuid.uuid4()), username=data['username'], email=data['email'], password=hashed_password)
+    user.save()
+    return jsonify({'message': 'User created in the database'})
+
