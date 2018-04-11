20b41485630c3feb269043689a19570058856373
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-04-12 02:55:16 +0300

    created order endpoints for created an order and showing all the orders

--- app/models.py
+++ app/models.py
@@ -7,7 +7,6 @@ class User(db.Model):
     username = db.Column(db.String, index=True, unique=True)
     email = db.Column(db.String, index=True, unique=True)
     password = db.Column(db.String(80))
-    orders = db.relationship('Order', order_by='Order.id', cascade="all, delete-orphan")
 
     """Method for saving user into the database user """
     def save(self):
@@ -21,13 +20,13 @@ class User(db.Model):
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(100), index=True, unique=True)
-    phone_number = db.Column(db.String(50),index=True, unique=True)
+    phone_number = db.Column(db.String(50), index=True, unique=True)
     how_to_contact = db.Column(db.String(50))
     leading_channel = db.Column(db.String(50))
     project_type = db.Column(db.String(50))
     software_requirements = db.Column(db.String(250))
     description = db.Column(db.Text)
-    created_by = db.Column(db.String, db.ForeignKey(User.public_id))
+    created_by = db.Column(db.String)
 
     """Method for saving order into the database user """
 
--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 import jwt
 from datetime import datetime, timedelta
-from app.models import User
+from app.models import User, Order
 from config import Config
-
+import logging
+from app.decorators import token_required
 
 
 @app.route('/')
@@ -16,7 +17,7 @@ def hello_world():
 
 
 @app.route('/auth/register', methods=['POST'])
-def register_user(current_user):
+def register_user():
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
     user = User(public_id=str(uuid.uuid4()), username=data['username'], email=data['email'], password=hashed_password)
@@ -52,6 +53,58 @@ def login():
         return jsonify({'token': token.decode('UTF-8')})
     return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic-realm="Login Required!"'})
 
+
+@app.route('/user', methods=['GET'])
+def get_current_user():
+    token = None
+    """Method for getting the current user"""
+    if 'x-access-token' in request.headers:
+        token = request.headers['x-access-token']
+
+    if not token:
+        return jsonify({'message': 'Token is missing'}), 401
+
+    try:
+        data = jwt.decode(token, Config.SECRET_KEY)
+        current_user = data['public_id']
+    except Exception as e:
+        logging.exception(e)
+        return jsonify({'message': 'Token is invalid'})
+
+    return jsonify({'current_user': current_user})
+
+
+@app.route('/order', methods=['POST'])
+def create_order():
+    data = request.get_json()
+    public_id = str(uuid.uuid4())
+    phone_number = data['phone_number']
+    how_to_contact = data['how_to_contact']
+    leading_channel = data['leading_channel']
+    project_type = data['project_type']
+    software_requirements = data['software_requirements']
+    description = data['description']
+    created_by = data['created_by']
+    order = Order(public_id=public_id, phone_number=phone_number, how_to_contact=how_to_contact,
+                  leading_channel=leading_channel, project_type=project_type,
+                  software_requirements=software_requirements, description=description, created_by=created_by)
+
+    order.save()
+    return jsonify({'message': 'order updated successfully'})
+
+
+@app.route('/order', methods=['GET'])
+def get_all_orders():
+    orders = Order.query.all()
+    output = []
+
+    for order in orders:
+        order_data = {}
+        order_data['public_id'] = order.public_id
+        order_data['description'] = order.description
+        output.append(order_data)
+
+    return jsonify({'orders': output})
 
 
 
